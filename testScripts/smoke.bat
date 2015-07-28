9b81dea977d74d869368df1df2e510926d7525a7
esilgard <esilgard@yahoo.com>
2015-07-27 18:10:32 -0700

    no code changes

--- testScripts/smoke.bat
+++ testScripts/smoke.bat
@@ -1,17 +1,17 @@
 @echo off
-python nlp_engine.py -f sampleData/smoke_test.nlp.tsv  -g lung -t pathology -o smoke_test.nlp.json
+python nlp_engine.py -f sampleData\transfer2.nlp.tsv  -g lung -t pathology -o transfer2.nlp.json
 
-if exist smoke_test.nlp.json goto outputJsonOK
+if exist transfer2.nlp.json goto outputJsonOK
 echo Smoke test fail, commit rejected. No output json file found in target directory
 exit 1
 
 :outputJsonOK
-if exist sampleData\smoke_test\CN-14-99991.txt goto refFileOK
-echo Smoke test fail, commit rejected. Reference file CN-14-99991.txt not found in directory sampleData\smoke_test
+if exist sampleData\transfer2\CN-14-99991.txt goto refFileOK
+echo Smoke test fail, commit rejected. Reference file CN-14-99991.txt not found in directory sampleData\transfer2
 exit 3
 
 :refFileOK
-del smoke_test.nlp.json
-rmdir /S /Q sampledata\smoke_test
+del transfer2.nlp.json
+rmdir /S /Q sampleData\transfer2
 
 exit 0
