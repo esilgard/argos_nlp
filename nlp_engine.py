f4c84bd8004ae6337c10ab763c501b252114e860
esilgard <esilgard@yahoo.com>
2015-12-10 10:55:36 -0800

    no significant changes, removed a couple commented out lines

--- nlp_engine.py
+++ nlp_engine.py
@@ -101,9 +101,7 @@ OUTPUT_DICTIONARY[gb.CNTL]["source"] = "document source"
 OUTPUT_DICTIONARY[gb.CNTL]["docDate"] = "doc date"
 OUTPUT_DICTIONARY[gb.CNTL]["processDate"] = str(datetime.today().isoformat())
 metadata = metadata.get(NLP_ENGINE_PATH, ARGUMENTS)
-#metadata, groupings = metadata.get(NLP_ENGINE_PATH, ARGUMENTS)
 OUTPUT_DICTIONARY[gb.CNTL]["metadata"]= metadata
-#OUTPUT_DICTIONARY[gb.CNTL]["groupings"]= groupings
 OUTPUT_DICTIONARY[gb.ERRS] = []
 OUTPUT_DICTIONARY[gb.REPORTS] = []
 
