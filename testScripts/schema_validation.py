f2da15cb50372e4369813ddc558094b246b89bd6
wlane <will@asklane.com>
2016-07-20 14:49:32 -0700

    minor file reformat

--- testScripts/schema_validation.py
+++ testScripts/schema_validation.py
@@ -2,16 +2,18 @@ import sys
 from jsonschema import validate
 import json
 
+
 def load_json(filepath):
     with open(filepath) as data_file:
         data = json.load(data_file)
         return data
 
+
 def main(args):
     # only let the script run if there are 2 arguments + the .py script arg
     #  or
     # 0 arguments, in which case default values will be attempted
-    if len(args) != 3 and len(args)!= 1:
+    if len(args) != 3 and len(args) != 1:
         print("Invalid number of arguments: " + str(len(args)))
         return 1
 
@@ -30,6 +32,7 @@ def main(args):
 
     # validate and print results: no error means the validation succeeded
     print(validate(sys_out, schema))
+
 
 if __name__ == '__main__':
     main(sys.argv)
