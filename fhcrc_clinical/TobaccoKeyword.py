38168d77045a294c2886d06aa11a34e3fb26c0c6
esilgard <esilgard@yahoo.com>
2016-07-20 14:24:48 -0700

    merging in changes from abbottLane&SpenDM

--- fhcrc_clinical/TobaccoKeyword.py
+++ fhcrc_clinical/TobaccoKeyword.py
@@ -3,17 +3,16 @@
 #
 # Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
 #
-
-
-from KeywordHit import KeywordHit
 import global_strings as gb
+from fhcrc_clinical.KeywordSearch.KeywordHit import KeywordHit
+from fhcrc_clinical.KeywordSearch.KeywordGlobals import *
 
 class TobaccoKeyword(KeywordHit):
-    ''' find tobacco/smoking related keywords '''
+    """ find tobacco/smoking related keywords """
     __version__ = 'TobaccoKeyword1.0'
+
     def __init__(self):
         super(TobaccoKeyword, self).__init__()
         self.field_name = 'TobaccoKeyword'
-        ## parentheses make the match group capturable
-        self.regex = r'(([Tt]obacco)|([sS]mok((e[sr]?)|(ing))))'
+        self.regex = KeywordHit.get_regex_from_file(TOBACCO)
         self.table = 'KeywordHitTable'
--- testScripts/smoke.bat
+++ testScripts/smoke.bat
@@ -1,17 +1,25 @@
 @echo off
-python nlp_engine.py -f sampleData\transfer2.nlp.tsv  -g lung -t pathology -o transfer2.nlp.json
+python ..\nlp_engine.py -f sampleData\transfer2.nlp.tsv  -g lung -t pathology -o transfer2.nlp.json
 
 if exist transfer2.nlp.json goto outputJsonOK
 echo Smoke test fail, commit rejected. No output json file found in target directory
 exit 1
 
 :outputJsonOK
-if exist sampleData\transfer2\CN-14-99991.txt goto refFileOK
+if exist sampleData\transfer2\CN-14-99991.txt goto validateJSON
 echo Smoke test fail, commit rejected. Reference file CN-14-99991.txt not found in directory sampleData\transfer2
 exit 3
+
+:validateJSON
+start python testScripts\schema_validation.py "..\schema.json" "transfer2.nlp.json"
+if errorlevel 1 goto exitOne
+goto refFileOK
 
 :refFileOK
 del transfer2.nlp.json
 rmdir /S /Q sampleData\transfer2
 
 exit 0
+
+:exitOne
+exit 1
