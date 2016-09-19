6c4bc9303099fe35d0e42d5d8439b1b94c4a8880
Silgard <esilgard@fhcrc.org>
2016-09-19 13:34:48 -0700

    fixing but in Cellularity algorithms (n=2) moved into heme specific directory

--- fhcrc_pathology/heme/CellularityPercent.py
+++ fhcrc_pathology/heme/CellularityPercent.py
@@ -5,21 +5,19 @@
 # Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
 #
 
-from OneFieldPerReport import OneFieldPerReport
+from ..OneFieldPerReport import OneFieldPerReport
 import global_strings as gb
 
-class CellularityType(OneFieldPerReport):
-    ''' extract the cellularity type (hypo, hyper, normal) '''
-    __version__ = 'CellularityType1.0'
-
+class CellularityPercent(OneFieldPerReport):
+    ''' extract the cellularity percentage(s)'''
+    __version__ = 'CellularityPercent1.0'
     def __init__(self):
-        super(CellularityType, self).__init__()
-        self.field_name = 'CellularityType'
+        super(CellularityPercent, self).__init__()
+        self.field_name = 'CellularityPercent'
         self.table = gb.PATHOLOGY_TABLE
         self.specimen_confidence = 0.85
         self.unlabled_specimen_confidence = 0.7
-        ## reference lists & dictionaries ##
-        self.file_name_string = 'cellularity'
-        self.regex = r'(.)'
+        ## relevant sections of the report ##
+        self.regex = r'Cellularity:[ ]+[\w\d ]*([1-9][\d]?([ \-]+[\d]{1,2})?)%'
         self.value_type = 'match'
         self.match_style = 'all'
